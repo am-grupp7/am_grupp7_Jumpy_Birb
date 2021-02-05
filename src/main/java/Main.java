079d69bbb044248d506bfbe47605dcd6e332f2d2
Fredrik <fredrikbach1@gmail.com>
2021-02-05 10:09:08 +0100

    add a demo window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		JFrame main = new JFrame("Hello");
+		main.setMinimumSize(new Dimension(400, 400));
+		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel myLabel = new JLabel("Hello world!", SwingConstants.CENTER);
+		myLabel.setFont(new Font("Arial", Font.BOLD, 25));
+		myLabel.setBackground(Color.gray);
+
+		main.getContentPane().add(myLabel);
+		main.setVisible(true);
+
 	}
 }

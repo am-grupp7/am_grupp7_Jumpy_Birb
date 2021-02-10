dd58d0d18aaa6f6be7ded775d79c8d0f68a0258b
Fredrik <fredrikbach1@gmail.com>
2021-02-10 20:25:03 +0100

    minor changes

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
-behövs ej?
+import {cvs, ctx} from './main.js';
+
+const bgSprite = new Image()
+bgSprite.src = "images/background.png";
+
+const background = {
+    sX : 0,
+    sY : 0,
+    w : 720,
+    h : 449,
+    x : 0,
+    y : 0,
+
+    draw : function() {
+        ctx.drawImage(bgSprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+}
+
+export {background};

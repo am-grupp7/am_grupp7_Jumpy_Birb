1a1a1741e14db1f63e346d09cad0a511a32ada1e
uzair <hej@uzair.se>
2021-02-11 21:42:57 +0100

    edited png files

--- pipes.js
+++ pipes.js
@@ -2,25 +2,37 @@ export class Pipes {
     constructor(cvs, ctx) {
         this.cvs = cvs;
         this.ctx = ctx;
+             
+        this.topsX = 0,
+        this.topsY = 0,
+        
+        this.topx = 500,
+        this.topy = -80,
 
-                
-        this.sX = 0,
-        this.sY = 0,
-        this.x = 500,
-        this.y = -80,
-        this.w = 100,
-        this.h = 249;
+        this.topw = 100,
+        this.toph = 249;
 
-        this.pipes = new Image();
-        this.pipes.src = "images/TopPipe.png"; 
+        this.pipeTop = new Image();
+        this.pipeTop.src = "images/TopPipe.png"; 
+      
+        this.btmsX = 0,
+        this.btmsY = 0,
+        
+        this.btmx = 500,
+        this.btmy = 240,
 
-        // Testing
+        this.btmw = 100,
+        this.btmh = 249;
 
-        //once more
+        this.pipeBottom = new Image();
+        this.pipeBottom.src = "images/BottomPipe.png"; 
+
     }
     
     draw() {
-      this.ctx.drawImage(this.pipes, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         
+      this.ctx.drawImage(this.pipeTop, this.topsX, this.topsY, this.topw, this.toph, this.topx, this.topy, this.topw, this.toph);
+      this.ctx.drawImage(this.pipeBottom, this.btmsX, this.btmsY, this.btmw, this.btmh, this.btmx, this.btmy, this.btmw, this.btmh);
+
     }
 }

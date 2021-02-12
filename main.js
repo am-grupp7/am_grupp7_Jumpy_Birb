de72e72b1c671f70d7685e362f6f3fd2383babc2
Erik Blidner <erik.blidner@gmail.com>
2021-02-12 09:40:57 +0100

    Re-added controls

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { Birb } from './birb.js';
 import { Background } from './background.js';
 import { Pipes } from './pipes.js';
 import { Foreground } from './foreground.js';
+import { Controls } from './controls.js';
 
 class Main {
     constructor(cvs, ctx) {
@@ -9,6 +10,7 @@ class Main {
         this.ctx = ctx;
         this.frames = 0;
         this.birb = new Birb(cvs, ctx);
+        this.controls = new Controls(cvs);
         this.pipes = new Pipes(cvs, ctx);
         this.background = new Background(ctx);
         this.foreground = new Foreground(ctx);

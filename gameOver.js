5633f1b7434111047facde6dc63f5beb9999cfa7
Fredrik <fredrikbach1@gmail.com>
2021-02-17 15:21:05 +0100

    add fancy png

--- gameOver.js
+++ gameOver.js
@@ -3,15 +3,15 @@ export class GameOver {
         this.ctx = ctx,
         this.cvs = cvs,           
         this.state = state,   
-        this.sX = 175,
-        this.sY = 228,
+        this.sX = 0,
+        this.sY = 0,
         this.x = cvs.width/2 - 225/2,
-        this.y = 90,
+        this.y = 100,
         this.w = 225,
-        this.h = 202;
+        this.h = 200;
 
         this.gameOverSprite = new Image();
-        this.gameOverSprite.src = "images/sprite.png"; 
+        this.gameOverSprite.src = "images/gameover.png"; 
     }
     
     draw(state) {

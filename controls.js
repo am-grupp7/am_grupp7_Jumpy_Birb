abdff7db650917059081df690b5053d04241732a
Simon Riemertzon <simon.riemertzon@gmail.com>
2021-02-22 14:51:55 +0100

    Solved rotation

--- controls.js
+++ controls.js
@@ -1,7 +1,12 @@
 export class Controls {
-  constructor(birb) {
+  constructor(birb, state) {
     document.body.addEventListener('keyup', event => {
       if (event.code === 'Space') {
+        console.log("state", state)
+        if(state.current == state.getReady ) {
+          state.current = 1;
+          console.log("Yipeee")
+        }
         birb.flap()
       }
     }) 

1140497ec1e6ece2c0dfbff9d171cb5a304e4720
uzair <hej@uzair.se>
2021-02-25 14:33:05 +0100

    added highscore

--- score.js
+++ score.js
@@ -13,19 +13,50 @@ export class Score {
             this.canvasContext.fillStyle = "white";
             this.canvasContext.strokeStyle = "#000";
             this.canvasContext.font = "35px Verdana";
-            this.canvasContext.fillText(this.score.value, this.canvas.width/2, 50);
-            this.canvasContext.strokeText(this.score.value, this.canvas.width/2, 50);
+            this.canvasContext.fillText(this.score.value, this.canvas.width / 2, 50);
+            this.canvasContext.strokeText(this.score.value, this.canvas.width / 2, 50);
         }
-        else if(this.state.current == this.state.over) {
+        else if (this.state.current == this.state.over) {
+
             this.canvasContext.fillStyle = "black";
             this.canvasContext.strokeStyle = "#000";
             this.canvasContext.font = "35px Verdana";
             this.canvasContext.fillText(this.score.value, 250, 215);
             this.canvasContext.strokeText(this.score.value, 250, 215);
 
+
+            let storedBestScore = localStorage.getItem("bestScore");
+            if (storedBestScore != null) {
+
+                if (this.score.value > parseInt(storedBestScore)) {
+                    localStorage.setItem("bestScore", this.score.value);
+
+                }
+                else {
+                    this.score.best = localStorage.getItem("bestScore");
+
+                }
+            }
+            else {
+                localStorage.setItem("bestScore", this.score.value);
+
+            }
             this.canvasContext.fillText(this.score.best, 370, 215);
             this.canvasContext.strokeText(this.score.best, 370, 215);
         }
+
+        else {
+            this.canvasContext.fillText(this.score.best, 370, 215);
+            this.canvasContext.strokeText(this.score.best, 370, 215);
+        }
+
+
+
     }
+
+
+
+
+
 }
 
